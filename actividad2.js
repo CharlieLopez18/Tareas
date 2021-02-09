843d512f49d070a3811ae62d93f7489c2820af7c
Carlos Lopez <carlosantoniolopezcarcamo@gmail.com>
2021-02-08 22:22:55 -0600

    first commit

---
+++ actividad2.js
@@ -0,0 +1,52 @@
+
+var mes = 10; 
+
+
+switch (mes){
+    
+    case 11:
+        console.log('Es Otoño');
+        break;
+    
+    case 10:
+        console.log('Es Otoño');
+        break;
+        
+    case 9:
+        console.log('Es Otoño');
+        break;
+
+    case 8:
+        console.log('Es Verano');
+        break;
+
+    case 7:
+        console.log('Es Verano');
+        break;
+
+    case 6: 
+        console.log('Es Verano');
+        break;
+
+    case 5:
+        console.log('Es Primavera');
+        break;
+
+    case 4:
+        console.log('Es Primavera');
+        break;
+
+    case 3:
+        console.log('Es Primavera');
+        break;
+
+    case 2:
+        console.log('Es Invierno');
+        break;
+    
+    case 1:
+        console.log('Es Invierno');
+        break;
+}
+
+alert("Carlos Antonio López Carcamo - 21000808")
